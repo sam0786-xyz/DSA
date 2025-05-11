814834ec4ec456c7ddcacd433e53e3f670db39e7
Mohammad Sameer <sam0786.xyz@icloud.com>
2025-05-11 10:58:04 +0530

    build: basics of c++

---
+++ basics/src/basic.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string.h>
+// int main(){
+//     // cout is used to print and std is library which contains cout and cin
+//     std::cout<<"Hello World!"<<std::endl; 
+//     // "\n" or "std::endl" is used for newline
+//     std::cout<<"Hello Sam"<<"\n"<<"Bhai";
+// }
+using namespace std; // replaces std 
+
+int main(){
+    // int c;
+    // cin >> c;
+    // cout << c;
+
+    // string s1; // only first word will be printed space will act as a deliminter
+    // cin >> s1;
+    // cout << s1;
+
+    // string str1;
+    // getline(cin, str1); // to get multiple strings using single string variable
+    // cout << str1;
+
+    //Check if a person is adult or not
+    int age;
+    cin>> age;
+    if (age>=18){
+        cout<<"You are adult and eligible to jail.";
+    }
+    else{
+        cout<<"You are not adult, so stay away from commiting crimes.";
+    }
+
+    // switch case
+    // Functions are set of code which performs something for you
+// Functions are used to modularise code
+// Functions are used to increase readability
+// Functions are used to use same code multiple times
+// void → which does not returns anything 
+// parameterised
+// non parameterised
+}
